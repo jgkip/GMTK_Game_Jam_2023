ccc2ae01092bd16b48c6e5fe6fd180013d4f5391
Joseph Gonzaga <49375310+jgkip@users.noreply.github.com>
2023-07-07 14:48:09 -0400

    enemy and player movement

--- Assets/Scripts/Capabilities/EnemyFollow.cs
+++ Assets/Scripts/Capabilities/EnemyFollow.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
+    [SerializeField] private Transform target;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 }
--- Assets/Scripts/Capabilities/Movement.cs
+++ Assets/Scripts/Capabilities/Movement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Transform target;
+    public float speed; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Item").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // if an item exists, go towards it
+        // else, random walk 
+        if (target != null)
+        {
+            MoveTowardsItem();
+        }
+    }
+
+    private void MoveTowardsItem()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 }
